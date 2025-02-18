f4d37078db9abfc07572b835c3fb0e85a964c29a
Andrew Kolev <alteraweb53@gmail.com>
2025-02-18 12:25:00 +0200

    --

--- src/Resources/views/components/ui-input/index.blade.php
+++ src/Resources/views/components/ui-input/index.blade.php
@@ -2,17 +2,26 @@
 	"type" => 'text'
 ])
 
-@if(View::exists('components.input.' . strtolower($type)) || file_exists($global_components_path . 'input/'. strtolower($type) . '.blade.php'))
-  @php $componentName = 'ui-input.' . strtolower($type); @endphp
-  @php $notExists = false; @endphp
-@else
-  @php $componentName = 'ui-input.text'; @endphp
-  @php $notExists = true; @endphp
-@endif
 
-<x-dynamic-component :component="$componentName" :type="$type" {{$attributes}}/>
-@if($notExists)
-  <div class="text-danger p-2">
-    {{ __('The field type ":type" does not have form component ', ['type' => $type]) }}
-  </div>
-@endif
+@if(View::exists('components.input.' . strtolower($type)))
+  @php($componentName = 'input.' . strtolower($type))
+  @php($notExists = false)
+@elseif(file_exists($global_components_path . 'ui-input/'. strtolower($type) . '.blade.php'))
+  @php($componentName = 'ui-input.' . strtolower($type))
+  @php($notExists = false)
+    @else
+      @php($componentName = 'ui-input.text')
+      @php($notExists = true)
+    @endif
+
+    @isset($componentName)
+      @dump($componentName)
+      <x-dynamic-component :component="$componentName" :type="$type" {{$attributes}}>
+        {{ $slot }}
+      </x-dynamic-component>
+      @if($notExists)
+        <div class="text-danger p-2">
+          {{ __('The field type ":type" does not have form component ', ['type' => $type]) }}
+        </div>
+      @endif
+    @endisset
