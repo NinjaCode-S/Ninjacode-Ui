a1e59c0f821f4b0a73ab68c91d9815488171a29a
Andrew Kolev <alteraweb53@gmail.com>
2025-02-19 17:32:45 +0200

    --

--- src/Resources/views/components/ui-input/select.blade.php
+++ src/Resources/views/components/ui-input/select.blade.php
@@ -12,23 +12,35 @@
 	"id" => null,
 	"simple" => false,
 	"multiple" => false,
+	"class" => '',
 ])
-@if($label)
-  <label class="@if(!!$required) required @endif" for="{{ $id }}">{!! __($label) !!}</label>
-@endif
-<select class="form-control" @if($multiple) multiple @endif name="{{ $name }}"
-        id="{{ $id }}" @required($required) {{ $attributes }}>
-  @if($placeholder)
-    <option value="">{{ __($placeholder) }}</option>
-  @endif
-  @if(isset($variants))
-    @foreach($variants as $v => $variant)
-      @if(@is_array($value))
-        <option
-            value="{{$v}}"@selected(@in_array($v, $value) || @in_array($variant, $value))>{{ __($variant) }}</option>
-      @else
-        <option value="{{ $simple ? $variant : $v}}" @selected($value == $v)>{{ __($variant) }}</option>
+
+@switch(@$framework)
+  @case('uikit')
+    @php($cls = implode(' ', ['uk-select', $class]))
+      @break
+      @default
+        @php($cls =implode(' ',  ['form-control', $class]))
+        @break
+      @endswitch
+
+
+      @if($label)
+        <label class="@if(!!$required) required @endif" for="{{ $id }}">{!! __($label) !!}</label>
       @endif
-    @endforeach
-  @endif
-</select>
+      <select @class($cls) @if($multiple) multiple @endif name="{{ $name }}"
+              id="{{ $id }}" @required($required) {{ $attributes }}>
+        @if($placeholder)
+          <option value="">{{ __($placeholder) }}</option>
+        @endif
+        @if(isset($variants))
+          @foreach($variants as $v => $variant)
+            @if(@is_array($value))
+              <option
+                  value="{{$v}}"@selected(@in_array($v, $value) || @in_array($variant, $value))>{{ __($variant) }}</option>
+            @else
+              <option value="{{ $simple ? $variant : $v}}" @selected($value == $v)>{{ __($variant) }}</option>
+            @endif
+          @endforeach
+        @endif
+      </select>
