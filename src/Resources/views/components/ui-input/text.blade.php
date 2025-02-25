048e87cf5c0ee5605c83dc858be27de889c16d68
Andrew Kolev <alteraweb53@gmail.com>
2025-02-25 16:02:47 +0200

    --

--- src/Resources/views/components/ui-input/text.blade.php
+++ src/Resources/views/components/ui-input/text.blade.php
@@ -11,7 +11,9 @@
 	"id" => generateId(6),
 	"class" => '',
 	"groupTextPre" => null,
+	"groupTextPreAttrs" => null,
 	"groupTextPost" => null,
+	"groupTextPostAttrs" => null,
 ])
 
 @if($label)
@@ -19,12 +21,12 @@
 @endif
 <div class="input-group">
     @isset($groupTextPre)
-        <span class="input-group-text">{{ $groupTextPre }}</span>
+        <span class="input-group-text" {!! $groupTextPreAttrs !!} >{{ $groupTextPre }}</span>
     @endisset
     <input @class(['form-control',$class]) type="{{ $attrType }}" name="{{ $name }}"
            @required(!!$required) value="{{ $value }}"
            placeholder="{{ $placeholder }}" id="{{ $id }}" {{ $attributes }}>
     @isset($groupTextPost)
-        <span class="input-group-text">{{ $groupTextPost }}</span>
+        <span class="input-group-text" {!! $groupTextPostAttrs !!} >{{ $groupTextPost }}</span>
     @endisset
 </div>
